430bea720475987a780ded0c63a4625affc8e9ad
Marko Matic <mail.marko.matic@gmail.com>
2022-05-04 08:12:11 +1000

    - Add version

--- src/os/index.js
+++ src/os/index.js
@@ -2,5 +2,6 @@ import { Platform } from 'react-native';
 
 export const OS = {
     isIOS: () => Platform.OS === 'ios',
-    isAndroid: () => Platform.OS === 'android'
+    isAndroid: () => Platform.OS === 'android',
+    version: () => Platform.Version
 };
